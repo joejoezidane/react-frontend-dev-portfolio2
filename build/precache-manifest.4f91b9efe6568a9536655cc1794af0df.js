b97965d94dc9215f3cd80826dbd357532268d8e4
Joel Hernandez <joel_99_sv@yahoo.com>
2021-08-25 19:16:14 -0600

    new build

---
+++ build/precache-manifest.4f91b9efe6568a9536655cc1794af0df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8af97893deed019f3b4f3fead32e22",
+    "url": "/react-frontend-dev-portfolio2/index.html"
+  },
+  {
+    "revision": "13d5e8ff8f9c10bf6fc6",
+    "url": "/react-frontend-dev-portfolio2/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "2ad28f724f1b39c8f641",
+    "url": "/react-frontend-dev-portfolio2/static/css/main.fcf2461e.chunk.css"
+  },
+  {
+    "revision": "13d5e8ff8f9c10bf6fc6",
+    "url": "/react-frontend-dev-portfolio2/static/js/2.ba3ba621.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio2/static/js/2.ba3ba621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad28f724f1b39c8f641",
+    "url": "/react-frontend-dev-portfolio2/static/js/main.a718e6b4.chunk.js"
+  },
+  {
+    "revision": "c4d2ebbd97e3382d2091",
+    "url": "/react-frontend-dev-portfolio2/static/js/runtime-main.c896395a.js"
+  }
+]);
